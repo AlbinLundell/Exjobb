04fbc8a5119c6b097a8d90a21c27dfe456663588
Niklas <niklask456gmail.com>
2022-02-16 13:36:49 +0100

    Update?

--- CorrelationMatrix/Correlation_matrix_Ma1c_4weeks.py
+++ CorrelationMatrix/Correlation_matrix_Ma1c_4weeks.py
@@ -1,10 +1,10 @@
 """
-Correlation matrix för,
-Matte 1C (För matte 2C, 3C och 4C kan man kolla på NP och tidigare mattebetyg också)
-Mål: Att upptäcka vilka variabler som är bra att analysera i jämförelse med betyg;
-Input: Canvasdata, fråvaro, diagnos matte, dignos svenska,
-12 elever får vi data på som ahr E eller F
-
+Correlation matrix för;
+Kurs: Matte1C
+Input tables: Betyg, Canvas, diagnoser, frånvaro
+Antal elever: 12 stycken
+Binary classification: E-F
+Datum (Canvas):
 """
 
 
@@ -31,9 +31,7 @@ df_betyg = pd.read_sql_query('SELECT * FROM betyg '
 df_betyg_1 = df_betyg.drop_duplicates(subset=['slumpkod'])
 
 # --------------------------------- Canvas Table -----------------------------------------------
-# Note, problems with data
-# Read on after 4 weeks,
-# One after 8 weeks, and 12 weeks
+#
 # ----------- Read the table -------------------------
 df_canvas = pd.read_sql_query('SELECT * FROM canvas '
                               'WHERE kurs LIKE "MATMAT01%%" AND datum LIKE "%%09-15" ', con = db_connection )
@@ -42,8 +40,6 @@ df_canvas_4weeks2 = df_canvas.drop_duplicates(subset = ['slumpkod'])
 
 # ------------------- Create two new columns ------------------
 # Måste först byta till "to_datetime", droppar "datum" för har inför en annany ny column
-# df2 = df.loc[:, ['A']]
-# loc[:, ('one', 'second')]
 df_canvas_4weeks2['datum_DateTime'] = pd.to_datetime(df_canvas_4weeks2.loc[:, ('datum')], format='%Y-%m-%d')
 df_canvas_4weeks2 = df_canvas_4weeks2.drop(columns = ["datum"])
 
@@ -56,8 +52,6 @@ df_canvas_4weeks2['Average_participation'] = df_canvas_4weeks2.groupby('kurs')['
 df_canvas_4weeks2['page_view_factor'] = df_canvas_4weeks2['page_views']/df_canvas_4weeks2['Average_page_views']
 # Create a new column, each student has a participations_factor
 df_canvas_4weeks2['participation_factor'] = df_canvas_4weeks2['participations']/df_canvas_4weeks2['Average_participation']
-
-
 
 
 
@@ -79,8 +73,7 @@ df_diagnos = pd.read_sql_query(sql_query_diagnos, con = db_connection)
 # print(df_diagnos)
 
 # --------------------------- Merge tables -------------------------------
-# betyg, Canvas, frånvaro, diagnos
-
+# Merge Betyg, Canvas, frånvaro, diagnos
 df_merge = df_betyg_1.merge(
     df_canvas_4weeks2.merge(
         df_franvaro.merge(
@@ -88,6 +81,7 @@ df_merge = df_betyg_1.merge(
         how ='inner', on = 'slumpkod')
     , how ='inner', on = 'slumpkod')
 
+# Droppa onödiga variabler
 df_merge_1 = df_merge.drop(columns = ['id_x', 'slumpkod', 'kurs', 'id_y', 'kurs_x', 'start', 'slut', 'status', 'max_page_views',
                                       'max_participations', 'tardiness_breakdown_floating', 'tardiness_breakdown_total',
                                       'kurs_y', 'Lektionstid', 'Läsår', 'Engelska Vocabulary', 'Engelska Grammar', 'Engelska Reading',
@@ -95,19 +89,25 @@ df_merge_1 = df_merge.drop(columns = ['id_x', 'slumpkod', 'kurs', 'id_y', 'kurs_
 
 
 # Måste ersätta betyg med siffror
+# Här måste koden se annorlunda ut om vi ska göra om alla ["A", "B", "C", "D", "E"] till "P"
 values = ["A", "B", "C", "D", "-"]
+
+# Droppar alla element i values lista
 df_dropped = df_merge_1[df_merge_1.betyg.isin(values) == False]
+# Byter ut F mot 0 och E mot 1
 df_dropped['betyg'] = df_dropped['betyg'].str.replace('F', '0')
 df_dropped['betyg'] = df_dropped['betyg'].str.replace('E', '1')
 df_dropped["betyg"] = df_dropped['betyg'].astype(int)
 
+# Drop even more
 df_more_dropped = df_dropped.drop(columns = ['tardiness_breakdown_late', 'tardiness_breakdown_missing', 'tardiness_breakdown_on_time'])
 
 print(df_more_dropped)
 # Har bara tolv elever kvar som har E eller F
 
 # ------------------------- Matrix -------------------------
-corrMatrix = df_more_dropped.corr(method = 'spearman')
+corrMatrix = df_more_dropped.corr()
+# Använder Pearson metoden
 # print(corrMatrix)
 sn.heatmap(corrMatrix, annot=True)
 plt.show()
