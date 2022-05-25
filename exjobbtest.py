4dc66fd35ec9836254ce2b5acd3d4035cba97309
Spoole67 <79168475+Spoole67@users.noreply.github.com>
2022-05-25 15:14:58 +0200

    Delete exjobbtest.py

--- exjobbtest.py
+++
@@ -1,7 +0,0 @@
-#Testprojekt för att se så saker funkar
-name=input("state your name")
-name= "Hello "+name
-print(name)
-print("update directly from pycharm")
-
-print("Test Niklas?--> Får jag upp detta!?!?")
